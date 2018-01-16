59193353e6694abd4c1358cf98a759f0346c2be2
josephlaycano <josephlaycano@cb5109-13.labs.sce.carleton.ca>
2018-01-16 15:31:16 -0500

    added code to client

--- assignment1/src/assignment1/client.java
+++ assignment1/src/assignment1/client.java
@@ -1,12 +1,40 @@
 package assignment1;
 import java.net.*;
+import java.io.*;
 
 public class client {
-	private DatagramSocket socket;
+	private DatagramSocket sendReceiveSocket;
+	private byte[] requests;
+	private DatagramPacket sendPacket;
 	
-	public static void main(String[] args) {
-		
-
+	public client() {
+		/* Construct a datagram socket and bind it to any available
+		 * port and on the local host machine. This socket will be used to 
+		 * send and receive UDP Datagram packets.
+		 */
+		try {
+			sendReceiveSocket = new DatagramSocket();
+			requests = new byte[20];
+		} catch (SocketException se) {
+			se.printStackTrace();
+			System.exit(1);
+		}
 	}
 
+	public void sendAndReceive() {
+		try {
+			for (int i = 0; i < 11; i++) {
+				requests = null;
+				sendPacket = new DatagramPacket(requests, requests.length, InetAddress.getLocalHost(), 23);
+			}
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	public static void main(String[] args) {
+		client c = new client();
+		
+	}
 }
--- assignment1/src/assignment1/host.java
+++ assignment1/src/assignment1/host.java
@@ -1,8 +1,19 @@
 package assignment1;
 import java.net.*;
+import java.io.*;
 
 public class host {
-
+	private DatagramSocket receiveSocket;
+	private DatagramSocket sendReceiveSocket;
+	
+	public host() {
+		try {
+			receiveSocket = new DatagramSocket();
+			sendReceiveSocket
+		} catch (SocketException se){
+			
+		}
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

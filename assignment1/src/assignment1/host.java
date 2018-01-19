3ecb9e366e275e93ef1bd55ec193a6778a74d53b
josephlaycano <joseph.laycano@gmail.com>
2018-01-18 22:20:52 -0500

    added host.java and server.java

--- assignment1/src/assignment1/host.java
+++ assignment1/src/assignment1/host.java
@@ -3,20 +3,42 @@ import java.net.*;
 import java.io.*;
 
 public class host {
-	private DatagramSocket receiveSocket;
-	private DatagramSocket sendReceiveSocket;
+	protected DatagramSocket receiveSocket, sendReceiveSocket, sendSocket;
+	protected DatagramPacket sendPacket, receivePacket;
 	
 	public host() {
 		try {
-			receiveSocket = new DatagramSocket();
-			//sendReceiveSocket
+			sendReceiveSocket = new DatagramSocket();
+			receiveSocket = new DatagramSocket(23);
+			sendSocket = new DatagramSocket();
 		} catch (SocketException se){
-			
+			se.printStackTrace();
+			System.exit(1);
 		}
 	}
+	
+	public void loop() {
+		byte[] data;
+		
+		while(true) {
+			data = new byte[100];
+			receivePacket = new DatagramPacket(data, data.length);
+			
+			// Attempt to retrieve data from socket
+			try {
+				System.out.println("Waiting to receive...");
+				receiveSocket.receive(receivePacket);
+			} catch (IOException e) {
+				System.out.println("Receive socket timed out. \n" + e);
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		host h = new host();
+		h.loop();
 	}
 
 }
--- assignment1/src/assignment1/server.java
+++ assignment1/src/assignment1/server.java
@@ -2,10 +2,19 @@ package assignment1;
 import java.net.*;
 
 public class server {
-	protected DatagramSocket receiveSocket;
+	protected DatagramSocket sendSocket, receiveSocket;
+	protected DatagramPacket sendPacket, receivePacket;
 	
+	public server() { 
+		try {
+			receiveSocket = new DatagramSocket(23);
+		} catch (SocketException se) {
+			se.printStackTrace();
+			System.exit(1);
+		}
+	}
 	
 	public static void main(String[] args) {
-
+		
 	}
 }

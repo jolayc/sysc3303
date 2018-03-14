9f03d275f25831f2ac0689548fe4275f5ccb6065
josephlaycano <joseph.laycano@gmail.com>
2018-03-13 22:02:18 -0400

    First commit

---
+++ src/iteration4/ErrorSimulator.java
@@ -0,0 +1,488 @@
+package iteration4;
+/**
+ * Error Simulator of a client/server TFTP application
+ * The Error Simulator receives a request packet from a client and then
+ * sends it to the server
+ * It then receives from the server a reply packet back to the original client
+ */
+import java.io.IOException;
+import java.net.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+
+public class ErrorSimulator {
+	
+	private final byte ZERO = 0;
+	private final byte ONE = 1;
+	private final byte TWO = 2;
+	private final byte THREE = 3;
+	private final byte FOUR = 4;
+	
+	private DatagramSocket receiveSocket, sendReceiveSocket;
+	private DatagramPacket receivePacket, sendReceivePacket, sendPacket;
+	private DatagramPacket simulatorPacket;
+	private static ErrorType type;
+	private static PacketType packet;
+	private static int packetNumber;
+	private static int delay;
+	private static int duplicateOffset;
+	private int count;
+	
+	/**
+	 * Constructor for host
+	 * creates a Datagram Socket to receive packets from client on port 23.
+	 * creates Datagram Socket to send and receive packets from server on any avalible port
+	 */
+	public ErrorSimulator(){
+		try {
+			//Constructs a socket to receive packets bounded to port 23
+			receiveSocket = new DatagramSocket(23);
+			
+			//Constructs a socket to send packets from any available port
+			sendReceiveSocket = new DatagramSocket();
+			
+			count = 0;
+			//Set timeout
+			//sendReceiveSocket.setSoTimeout(5000);
+		} catch (SocketException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	/**
+	 * Receives a request packet from the client
+	 * Sends the request to the server
+	 * Receives a response packet from the server
+	 * Sends the response to the client 
+	 */
+	private void receiveAndSend() {
+		if(type.name().equals("NORMAL_OPERATION")) {
+			// buffers for send and receive packets
+			byte[] receiveData = new byte[512 + 4];
+			byte[] sendData = new byte[512 + 4];
+			
+			// status flag
+			boolean stop = false;
+			
+			// port number
+			// 69 for RQ, 23 for DATA and ACK
+			int port = 69;
+			
+			// repeat forever
+			while(true) {
+				// CLIENT TO SERVER
+				// receive packet from client
+				receivePacket = new DatagramPacket(receiveData, receiveData.length);
+				receivePack(receiveSocket, receivePacket);
+				
+				// check if finished sending and receiving between client and server
+				if (receivePacket.getData()[1] == 3 && receivePacket.getData()[515] == 0) {
+					stop = true;
+					port = 69;
+				}
+				
+				// send receive packet from client to server
+				// the first packet (which should be a RQ) should be sent to port 69
+				try {
+					sendReceivePacket = new DatagramPacket(receivePacket.getData(), receivePacket.getLength(), InetAddress.getLocalHost(), port);
+				} catch (UnknownHostException e1) {
+					e1.printStackTrace();
+					System.exit(1);
+				}
+				
+				// sends the sendReceivePacket to the server
+				sendPack(sendReceiveSocket, sendReceivePacket);
+				printSend(sendReceivePacket);
+				
+				// SERVER TO CLIENT
+				// Requests should be sent to port 69
+				if (!stop) {
+					try {
+						sendReceivePacket = new DatagramPacket(sendData, sendData.length, InetAddress.getLocalHost(), port);
+					} catch (UnknownHostException e1) {
+						e1.printStackTrace();
+						System.exit(1);
+					}
+					
+					// waits until sendReceivePacket receives a packet from the server
+					receivePack(receiveSocket, sendReceivePacket);
+					printReceive(sendReceivePacket);
+					
+					// reassign port number to Client port
+					port = receivePacket.getPort();
+					
+					// send the packet
+					sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), port);
+					sendPack(sendReceiveSocket, sendPacket);
+					printSend(sendPacket);
+				}
+			}	
+		} else if (type.name().equals("LOSE_PACKET")) {
+			simulateLosePacket();
+			type = ErrorType.getErrorType(0); // after done simulating error, swap type to normal operation and rerun receiveAndSend
+			receiveAndSend();
+		} else if (type.name().equals("DELAY_PACKET")) {
+			simulateDelayPacket();
+			type = ErrorType.getErrorType(0);
+			receiveAndSend();
+		} else if (type.name().equals("DUPLICATE_PACKET")) {
+			/* FIX THIS */
+			simulateDuplicatePacket(simulatorPacket.getData(), sendReceiveSocket);
+			type = ErrorType.getErrorType(0);
+			receiveAndSend();
+		}
+	}
+	
+	
+	/*
+	 * A method that throws away the packet sent
+	 */
+	private void simulateLosePacket() {
+		findPacket();
+		System.out.println("ErrorSim: Dropping packet...");
+		// don't send the packet/do nothing to force a timeout
+	} 
+	
+	private void simulateDuplicatePacket(byte[] data, DatagramSocket socket) {
+		findPacket();
+		DatagramPacket duplicate = simulatorPacket;
+		
+		packetNumber = duplicateOffset - packetNumber;
+		findPacket();
+		
+		if(data[1] == THREE && packet.name().equals("DATA")) {
+			System.out.println("ErrorSim: Sending a duplicate packet...");
+			try {
+				socket.send(duplicate);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+		if(data[1] == FOUR && packet.name().equals("ACK")) {
+			System.out.println("ErrorSim: Sending a duplicate packet...");
+			try {
+				socket.send(duplicate);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+	}
+	
+	private void simulateDelayPacket() {
+		findPacket();
+		System.out.println("ErrorSim: Delaying packet...");
+		try {
+			TimeUnit.SECONDS.sleep(delay);
+			sendReceiveSocket.send(simulatorPacket);
+		} catch (InterruptedException ie) {
+			// sleep interrupted
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+	
+	/**
+	 * Find packet to simulate error and save it in simulatorPacket
+	 */
+	private void findPacket() {
+		// buffers for send and receive packets
+		byte[] receiveData = new byte[512 + 4];
+		byte[] sendData = new byte[512 + 4];
+					
+		// status flag
+		boolean stop = false;
+		boolean firstSend = true;
+					
+		// port number
+		// 69 for RQ, 23 for DATA and ACK
+		int port = 69;
+		int count = 0;
+					
+		// repeat forever
+		while(true) {
+			// CLIENT TO SERVER
+			// receive packet from client
+			receivePacket = new DatagramPacket(receiveData, receiveData.length);
+			receivePack(receiveSocket, receivePacket);
+						
+			// check if finished sending and receiving between client and server
+			if (receivePacket.getData()[1] == 3 && receivePacket.getData()[515] == 0) {
+				stop = true;
+				port = 69;
+			}
+				
+			if(!firstSend) count++;
+			firstSend = false;
+			
+			if(receivePacket.getData()[1] == THREE && packet.name().equals("DATA")) {
+				if(count == packetNumber) simulatorPacket = receivePacket;
+			}
+			
+			if(receivePacket.getData()[1] == FOUR && packet.name().equals("ACK")) {
+				if(count == packetNumber) simulatorPacket = receivePacket;
+			}
+			
+			// send receive packet from client to server
+			// the first packet (which should be a RQ) should be sent to port 69
+			try {
+				sendReceivePacket = new DatagramPacket(receivePacket.getData(), receivePacket.getLength(), InetAddress.getLocalHost(), port);
+			} catch (UnknownHostException e1) {
+				e1.printStackTrace();
+				System.exit(1);
+			}
+						
+			// sends the sendReceivePacket to the server
+			sendPack(sendReceiveSocket, sendReceivePacket);
+			printSend(sendReceivePacket);
+						
+			// SERVER TO CLIENT
+			if (!stop) {
+				try {
+					sendReceivePacket = new DatagramPacket(sendData, sendData.length, InetAddress.getLocalHost(), port);
+				} catch (UnknownHostException e1) {
+					e1.printStackTrace();
+					System.exit(1);
+				}
+							
+				// waits until sendReceivePacket receives a packet from the server
+				receivePack(sendReceiveSocket, sendReceivePacket);
+				count++;
+				
+				if(receivePacket.getData()[1] == THREE && packet.name().equals("DATA")) {
+					if(count == packetNumber) simulatorPacket = receivePacket;
+				}
+				
+				if(receivePacket.getData()[1] == FOUR && packet.name().equals("ACK")) {
+					if(count == packetNumber) simulatorPacket = receivePacket;
+				}
+							
+				// this should change the port to 23
+				port = receivePacket.getPort();
+							
+				// send the packet
+				sendPacket = new DatagramPacket(sendData, sendData.length, receivePacket.getAddress(), port);
+				sendPack(sendReceiveSocket, sendPacket);
+				}
+			}	
+	}
+	
+// NOT NEEDED AT THE MOMENT	
+//	private boolean checkError(DatagramSocket socket) {
+//		if(receivePacket.getData()[1] == THREE || receivePacket.getData()[1] == FOUR) {
+//			if(count == packetNumber) {
+//				if(packet.name().equals("DATA") && receivePacket.getData()[1] == THREE) {
+//					simulateError(socket);
+//					return true;
+//				}
+//				if(packet.name().equals("ACK") && receivePacket.getData()[1] == FOUR) {
+//					simulateError(socket);
+//					return true;
+//				}
+//			}
+//		}return false;
+//	}
+	
+//	/** NOT NEEDED AT THE MOMENT
+//	 * A method used to check if an error is being simulated
+//	 * @return	true if an error has been simulated, false otherwise
+//	 */
+//	private void simulateError(DatagramSocket socket) {
+//		switch(type.name()) {
+//			case "LOSE_PACKET":
+//				simulateLosePacket(); //changed simulateLosePacket(socket);
+//			case "DUPLICATE_PACKET":
+//				simulatorPacket = receivePacket;
+//			case "DELAY_PACKET": 
+//				simulatorPacket = receivePacket;
+//				simulateDelayPacket();
+//		}
+//	}
+	
+	/**
+	 * Sends a packet to a socket
+	 * @param socket, DatagramSocket where the packet will be sent
+	 * @param packet, DatagramPacket that will be sent
+	 */
+	public void sendPack(DatagramSocket socket, DatagramPacket packet) {
+	
+		try{
+			 socket.send(packet);
+		 }
+		 catch(IOException io){
+			 io.printStackTrace();
+			 System.exit(1);
+		 }
+		
+	}
+	
+	/**
+	 * Receives a packet from a socket
+	 * @param socket, DatagramSocket where the packet data will be received from
+	 * @param packet, DatagramPacket where the data from the socket will be stored
+	 */
+	public void receivePack(DatagramSocket socket, DatagramPacket packet) {
+		
+		System.out.println("Host: Waiting for Packet.\n");
+		try {        
+	         socket.receive(packet);
+	    } catch (IOException e) {
+	         e.printStackTrace();
+	         System.exit(1);
+	    }
+		
+		printReceive(packet);
+	}
+	
+	/**
+	 * Prints information relating to a send request
+	 * @param packet, DatagramPacket that is used in the send request
+	 */
+	private void printSend(DatagramPacket packet){
+		System.out.println("Host: Sending packet");
+	    System.out.println("To host: " + packet.getAddress());
+	    System.out.println("Destination host port: " + packet.getPort());
+	    printStatus(packet);
+	}
+	
+	/**
+	 * Prints information relating to a receive request
+	 * @param packet, DatagramPacket that is used in the receive request
+	 */
+	private void printReceive(DatagramPacket packet){
+		System.out.println("Host: Packet received");
+	    System.out.println("From host: " + packet.getAddress());
+	    System.out.println("Host port: " + packet.getPort());
+	    printStatus(packet);
+	}
+	
+	/**
+	 * Prints information relating to a any request
+	 * @param packet, DatagramPacket that is used in the request
+	 */
+	private void printStatus(DatagramPacket packet){
+	    int len = packet.getLength();
+	    System.out.println("Length: " + len);
+	    System.out.print("Containing: ");
+	    
+	    //prints the bytes of the packet
+	    System.out.println(Arrays.toString(packet.getData()));
+	    
+	    //prints the packet as text
+		String received = new String(packet.getData(),0,len);
+		System.out.println(received);
+	}
+	
+	public static void main(String args[]){
+		ErrorSimulator sim = new ErrorSimulator();
+		Scanner sc = new Scanner(System.in);
+		boolean validType = false;
+		boolean validPacket = false;
+		
+		while(!validType) {
+			System.out.println("ErrorSim: Enter 0 for normal operation, 1 for lose a packet, 2 for delay a packet and 3 for duplicate a packet.");
+			while(!sc.hasNextInt()) sc.next();
+			int errorType = sc.nextInt();
+			if(errorType == 0) {
+				type = ErrorType.getErrorType(0);
+				validType = true;
+				validPacket = true;
+				packet = PacketType.getPacketType(0);
+			}
+			if(errorType >= 0 && errorType <= 3) {//if errorType is a valid ordinal for PacketType
+				type = ErrorType.getErrorType(errorType);
+				validType = true;
+			}
+			else System.out.println("ErrorSim: Invalid type entered");
+		}
+		
+		while(!validPacket) {
+			System.out.println("ErrorSim: Enter 0 for RRQ, 1 for WRQ, 2 for DATA, 3 for ACK");
+			while(!sc.hasNextInt()) sc.next();
+			
+			int packetType = sc.nextInt();
+			if(packetType >= 0 && packetType <= 3) {//if packetType is a valid ordinal for PacketType
+				packet = PacketType.getPacketType(packetType);
+				validPacket = true;
+			}
+			else System.out.println("ErrorSim: Invalid request entered");
+		}
+		
+		
+		if(packet.ordinal() == 2 || packet.ordinal() == 3) {//to determine the nth. DATA or ACK packet
+			if(packet.ordinal() == 2 ) System.out.println("ErrorSim: Enter the DATA packet that will be affected: ");
+			else System.out.println("ErrorSim: Enter the ACK packet that will be affected: ");
+			
+			boolean positive = false;
+			while(!positive) {
+				while(!sc.hasNextInt()) sc.next();
+				packetNumber = sc.nextInt();
+				if(packetNumber >= 0) positive = true;
+				else System.out.println("ErrorSim: Number must be greater than 0");
+			}
+		}
+		
+		if(type.ordinal() == 2) {//for delay packet
+			System.out.println("ErrorSim: Enter the delay, in seconds: ");
+			while(!sc.hasNextInt()) sc.next();
+			delay = 1000 * sc.nextInt();
+		}
+		
+		if(type.ordinal() == 3) {//for duplicate packet
+			System.out.println("ErrorSim: Enter the duplicate offsets: ");
+			while(!sc.hasNextInt()) sc.next();
+			duplicateOffset = sc.nextInt();
+		}
+		System.out.println("ErrorSim: Running...");
+		sim.receiveAndSend();
+		sc.close();
+	}
+	
+	public enum ErrorType {
+		
+		NORMAL_OPERATION(0),
+		LOSE_PACKET(1),
+		DELAY_PACKET(2),
+		DUPLICATE_PACKET(3);
+		private int type;
+		
+		ErrorType(int type){
+			this.type = type;
+		}
+		
+		static ErrorType getErrorType(int type) throws IllegalArgumentException {
+			if(type == 0) return NORMAL_OPERATION;
+			if(type == 1) return LOSE_PACKET;
+			if(type == 2) return DELAY_PACKET;
+			if(type == 3) return DUPLICATE_PACKET;
+			else throw new IllegalArgumentException("Invalid type");
+		}
+	}
+	
+	public enum PacketType {
+		
+		RRQ(0),
+		WRQ(1),
+		DATA(2),
+		ACK(3);
+		private int packet;
+		
+		PacketType(int packet){
+			this.packet = packet;
+		}
+		
+		static PacketType getPacketType(int packet) throws IllegalArgumentException {
+			if(packet == 0) return RRQ;
+			if(packet == 1) return WRQ;
+			if(packet == 2) return DATA;
+			if(packet == 3) return ACK;
+			else throw new IllegalArgumentException("Invalid request");
+		}
+	}
+
+}
---
+++ src/iteration4/ServerThread.java
@@ -0,0 +1,351 @@
+package iteration4;
+import java.net.*;
+import java.nio.file.FileAlreadyExistsException;
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+
+/**
+ * A server thread that will handle read/write requests received by server, sent by client 
+ */
+
+public class ServerThread extends Thread implements Runnable {
+	
+	private DatagramSocket sendReceiveSocket;
+	private DatagramPacket receivePacket, sendPacket;
+	// The directory where files will be written to
+	private String relativePath = System.getProperty("user.dir");
+	private Writer writer;
+	private int[] blockNumber;
+	
+	// String identifiers
+	private String message;
+	private String read = "READ";
+	private String write = "WRITE";
+	private File file;
+	
+	private byte[] path;
+	private int[] blockNum;
+	private int numberOfTimeout=0;
+	private boolean done = false;
+	
+	private final byte ZERO = 0x00;
+	private final byte ONE = 0x01;
+	private final byte TWO = 0x02; 
+	private final byte FOUR = 0x04;
+	/**
+	 * Constructor for ServerThread
+	 * @param receivePacket packet received from host
+	 * @param path is the file name that the server writing to 
+	 * @param message is the read or write request
+	 * @param blockNumber is the current block number of the file 
+	 */
+
+	public ServerThread(DatagramPacket receivePacket, byte[] path, File file, String message, int[] blockNumber) {
+		this.message = message;
+		this.receivePacket = receivePacket;
+		this.file = file;
+		this.path = path;
+		this.blockNumber = blockNumber;
+	}
+	/**
+	 * Run handles the packets received from the host. 
+	 * Request is processed by the Server but the DATA and ACK
+	 * packets are handled by the Thread
+	 */
+	public void run() {
+		try {
+			sendReceiveSocket = new DatagramSocket();
+			sendReceiveSocket.setSoTimeout(5000);
+		} catch (SocketException e1) { 
+			e1.printStackTrace();
+			System.exit(1);
+		}
+		
+		byte[] data =  receivePacket.getData();
+		if (data[0] == 0 && data[1] == 1) handleRead(); // read request
+		else if (data[0] == 0 && data[1] == 2) handleWrite(); // write request
+	}
+	
+	private void handleWrite() {
+		// response packet
+		byte[] response = new byte[512 + 4];
+		byte[] data;
+
+		// send ACK to write request
+		response = createACKPacket();
+		try {
+			sendPacket = new DatagramPacket(response, response.length, InetAddress.getLocalHost(), receivePacket.getPort());
+		} catch (UnknownHostException ue) {
+			ue.printStackTrace();
+			System.exit(1);
+		}
+		try {
+			sendReceiveSocket.send(sendPacket);
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		
+		// process DATA packets
+		while (true) {
+			data = new byte[512 + 4];
+			receivePacket = new DatagramPacket(data, data.length);
+			// receive packet from Client
+			try { 
+				sendReceiveSocket.receive(receivePacket);
+			}catch (SocketTimeoutException se){
+				numberOfTimeout++;
+				
+				if (numberOfTimeout==2){
+					try {
+						sendReceiveSocket.send(sendPacket);
+						numberOfTimeout=0;
+					} catch (IOException e) {
+						e.printStackTrace();
+						System.exit(1);
+						}
+					}
+				}
+			catch(IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			try {
+				handleData(receivePacket.getData());
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			// create ACK packet to acknowledge DATA packet
+			response = createACKPacket();
+			blockNum = calcBlockNumber();
+			try {
+				sendPacket = new DatagramPacket(response, response.length, InetAddress.getLocalHost(), receivePacket.getPort());
+			} catch (UnknownHostException ue) {
+				ue.printStackTrace();
+				System.exit(1);
+			}
+			try {
+				sendReceiveSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+			// check if end of write
+			int len = 0;
+			for(byte b: data){
+				if(b == 0 && len > 4) break;
+				len++;
+			}
+			
+			if(len < 512) {
+				break;
+			}
+		}
+		sendReceiveSocket.close();
+	}
+	
+	private void handleRead() {
+		// response packet
+			byte[] data = new byte[512 + 4];
+			byte[] response;
+				
+			// send DATA to read request
+			data = createDataPacket();
+			try {
+				sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 23);
+			} catch (UnknownHostException ue) {
+				ue.printStackTrace();
+				System.exit(1);
+			}
+			try {
+				sendReceiveSocket.send(sendPacket);
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+			}
+				
+			// process ACK packets
+			while (true) {
+				response = new byte[512 + 4];
+				receivePacket = new DatagramPacket(response, response.length);
+				// receive packet from Client
+				try { 
+					sendReceiveSocket.receive(receivePacket);
+				} catch (SocketTimeoutException se){
+					numberOfTimeout++;
+					if (numberOfTimeout==6){
+						try {
+							sendReceiveSocket.send(sendPacket);
+							numberOfTimeout=0;
+						} catch (IOException e) {
+							e.printStackTrace();
+							System.exit(1);
+						}
+					}
+				}
+				catch(IOException e) {
+				e.printStackTrace();
+				System.exit(1);
+				}
+				// create DATA packet after receiving ACK packet
+				data = createDataPacket();
+				blockNum = calcBlockNumber();
+				try {
+					sendPacket = new DatagramPacket(data, data.length, InetAddress.getLocalHost(), 23);
+				} catch (UnknownHostException ue) {
+					ue.printStackTrace();
+					System.exit(1);
+				}
+				try {
+					sendReceiveSocket.send(sendPacket);
+				} catch (IOException e) {
+					e.printStackTrace();
+					System.exit(1);
+				}					
+				// check if end of write
+				int len = 0;
+				for(byte b: response){
+					if(b == 0 && len > 4) break;
+					len++;
+				}
+					
+				if(len < 512) {
+					break;
+						
+				}
+			sendReceiveSocket.close();
+			}
+	}
+
+	/**
+	 * Creates a file (on machine) with requested file name to be written to
+	 * if it does not already exist
+	 * @param data
+	 */
+	private void handleData(byte[] data) throws IOException {
+		byte[] cleanedData = new byte[data.length - 4];
+		try {
+			writer = new Writer(file.getPath(), true);
+			for(int i = 0; i < cleanedData.length; i++) {
+				if(data[i+4] == 0) break;
+				cleanedData[i] = data[4+i];
+			}
+			writer.write(cleanedData);
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+		if (data.length < 516) {
+			//writer.close();
+			System.out.println("Server: File transfer/write complete.");
+
+		}
+	}
+	
+	
+	/**
+	 * increments block number 
+	 * @return block number as array on integers
+	 */
+	
+	private int[] calcBlockNumber(){
+
+		if(blockNumber[1] == 9) {
+			blockNumber[0]++;
+			blockNumber[1] = 0;
+		}
+		else{
+			blockNumber[1]++;
+		}
+
+		return blockNumber;
+	}
+	
+	/**
+	 * Create a data packet
+	 * @return byte[516] data packet
+	 */
+	public byte[] createDataPacket() {
+		byte[] data = new byte[512 + 4];
+		data[0] = 0;
+		data[1] = 3;
+		data[2] = (byte)blockNumber[0];
+		data[3] = (byte)blockNumber[1];
+		
+		int multiplier = 0;
+		if(blockNumber[1] > 1) multiplier += blockNumber[1]-1;
+		if(blockNumber[0] > 0) multiplier += (10*blockNumber[0]);
+
+	
+		for(int i = 0; i < path.length; i++){
+			if(path.length <= (512*multiplier+i)) break;
+			if((path.length > (512*multiplier+i)) && i < 512){
+				path[i] = path[512*multiplier+i];
+			}
+		}
+		
+		for(int j = 0; j < path.length; j++){
+			if(path.length <= (512*multiplier+j)) break;
+			if(j == 512) break;
+			data[4+j] = path[j];	
+		}	
+		return data;
+	}
+	
+	/**
+	 * Create an acknowledge packet containing {0,4,0,0}
+	 * @return byte[4] acknowledge packet
+	 */
+	public byte[] createACKPacket() {
+		byte[] data = new byte[4];
+		data[0] = 0;
+		data[1] = 4;
+		data[2] = (byte)blockNumber[0];
+		data[3] = (byte)blockNumber[1];
+		
+		return data;
+	}
+	
+	public byte[] createACKPacket(int[] block) {
+		byte[] pack = new byte[4];
+		// {0, 4} op code
+		pack[0] = ZERO;
+		pack[1] = FOUR;
+		// load block number into ack packet
+		pack[2] = (byte)block[0];
+		pack[3] = (byte)block[1];
+				
+		return pack;
+	}
+	
+	/**
+	 *  Print information relating to send request 
+	 * @param dp datagram Packet being printed
+	 */
+	private void printSend(DatagramPacket dp) {
+		System.out.println("Server: Sending packet");
+		System.out.println("To host: " + dp.getAddress());
+		System.out.println("Destination Port: " + dp.getPort());
+		printInfo(dp);
+	}
+	
+	/**
+	 *  Print information relating to packet
+	 * @param dp datagram Packet being printed
+	 */
+	private void printInfo(DatagramPacket dp) {
+		int len = dp.getLength();
+		System.out.println("Length: " + len);
+		System.out.println("Containing: ");
+
+		// prints the contents of packet as bytes
+		System.out.println(Arrays.toString(dp.getData()));
+		// prints the contents of packet as a String
+		String contents = new String(dp.getData(),0,len);
+		System.out.println(contents);
+	}
+}
